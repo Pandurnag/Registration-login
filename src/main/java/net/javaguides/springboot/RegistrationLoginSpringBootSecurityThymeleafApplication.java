66fdceafcfd0a45dfdcebebef253413ce32ca2bf
Pandurnag <96949058+Pandurnag@users.noreply.github.com>
2022-06-10 15:26:51 +0530

    registration login spring boot security thymeleaf

---
+++ src/main/java/net/javaguides/springboot/RegistrationLoginSpringBootSecurityThymeleafApplication.java
@@ -0,0 +1,21 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@SpringBootApplication
+@EnableScheduling
+public class RegistrationLoginSpringBootSecurityThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLoginSpringBootSecurityThymeleafApplication.class, args);
+	}
+
+}
